32ed1c07363481660b528abd271ac5debdc5ab14
Thomas Ourliac <thomas.ourliac@gmail.com>
2021-10-29 10:15:45 +0200

    1st commit TP1

--- activites/src/main/java/com/gmail/ourliac/thomas/activites/MainActivity.kt
+++ activites/src/main/java/com/gmail/ourliac/thomas/activites/MainActivity.kt
@@ -2,33 +2,35 @@ package com.gmail.ourliac.thomas.activites
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.gmail.ourliac.thomas.activites.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var clickButton: Button
-    private lateinit var textView: TextView
-    private lateinit var computeButton: Button
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
     private var nbClick = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        clickButton = findViewById(R.id.btn_click_me)
-        computeButton = findViewById(R.id.btn_compute)
-        textView = findViewById(R.id.text_view)
-
-        computeButton.setOnClickListener {
-            val intent = Intent(baseContext, ComputeActivity::class.java)
-            startActivity(intent)
-        }
-
-        clickButton.setOnClickListener {
-            nbClick++
-            textView.text = "Vous avez cliqué $nbClick fois"
-            clickButton.isEnabled = nbClick <= 4
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        with(binding) {
+            btnClickMe.setOnClickListener(this@MainActivity)
+            btnCompute.setOnClickListener(this@MainActivity)
         }
     }
 
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.btn_click_me -> {
+                nbClick++
+                binding.textView.text = getString(R.string.you_click_me, nbClick)
+                binding.btnClickMe.isEnabled = nbClick <= 4
+            }
+            R.id.btn_compute -> {
+                val intent = Intent(baseContext, ComputeActivity::class.java)
+                startActivity(intent)
+            }
+        }
+    }
 }
